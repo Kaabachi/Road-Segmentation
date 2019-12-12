43f7aa8e3474c8b40581554c3b3a16bfdd3d93b7
tariq <t.kalim.95@gmail.com>
2019-12-12 11:26:35 +0100

    add fcn resnet 50

--- models.py
+++ models.py
@@ -10,3 +10,4 @@ NUM_CLASSES = 2
 
 
 fcn_resnet101 = models.segmentation.fcn_resnet101(pretrained=PRETRAINED, num_classes=NUM_CLASSES)
+fcn_resnet50 = models.segmentation.fcn_resnet50(pretrained=PRETRAINED, num_classes=NUM_CLASSES)
