28b427116923dfb029de12cf3ae2723953b2e7ba
tariq <t.kalim.95@gmail.com>
2019-12-12 08:53:25 +0100

    models file

---
+++ models.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+from torchvision import models
+
+
+
+#C_DIM, X_DIM, Y_DIM = 3, 96, 96
+
+PRETRAINED = False
+NUM_CLASSES = 2
+
+
+fcn_resnet101 = models.segmentation.fcn_resnet101(pretrained=PRETRAINED, num_classes=NUM_CLASSES)
