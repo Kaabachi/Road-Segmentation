27e881d6b40ba43deacb2c1b7f812dce4b2266ae
gkarray <ghassen.karray@epfl.ch>
2019-12-15 01:54:34 +0100

    predict fixed to save images as it loops not after its finished

--- predict.py
+++ predict.py
@@ -31,9 +31,16 @@ def save_files(masks):
     mask = torch.tensor(masks, dtype=torch.float)
     
     for i in range(mask.size()[0]):
-        print(mask[i])
         img = transforms.ToPILImage()(mask[i])
         img.save(prediction_data_dir+"img" + str(i) + ".png","PNG")
+        
+def save_image(image):
+    prediction_data_dir = './Datasets/predictions/'
+    
+    mask = torch.tensor(image, dtype=torch.float)
+    
+    img = transforms.ToPILImage()(mask[i])
+    img.save(prediction_data_dir+"img" + str(i) + ".png","PNG")
 
 def crop(image):
     return image[PADDING:PADDING+PATCH_SIZE, PADDING:PADDING+PATCH_SIZE]
@@ -41,9 +48,10 @@ def crop(image):
 def predict(model, dataloader):
     model.eval()
     
-    result = np.zeros((len(dataloader),PATCH_SIZE,PATCH_SIZE), dtype=np.int32)
-    tmp = np.zeros((len(dataloader),LARGE_PATCH_SIZE,LARGE_PATCH_SIZE))
-    indices = np.zeros((len(dataloader), 3), dtype=int)
+#     result = np.zeros((len(dataloader),PATCH_SIZE,PATCH_SIZE), dtype=np.int32)
+#     indices = np.zeros((len(dataloader), 3), dtype=int)
+    
+    tmp_img = np.zeros((IMAGE_SIZE, IMAGE_SIZE))
     
     for ind_batch, sample_batched in enumerate(dataloader):
         batch_images = sample_batched["image"]
@@ -52,10 +60,30 @@ def predict(model, dataloader):
         final = output[0].detach()
         final[final>0.5] = 1
         final[final<=0.5] = 0
-                
-        tmp[ind_batch] = np.array(final[0][0])
-        result[ind_batch] = crop(tmp[ind_batch])
-        indices[ind_batch] = np.array([sample_batched['id'], sample_batched['x'], sample_batched['y']])
+        
+#         result[ind_batch] = crop(tmp[ind_batch])
+#         indices[ind_batch] = np.array([sample_batched['id'], sample_batched['x'], sample_batched['y']])
+        
+        tmp = np.array(final[0][0])
+        small_patch = crop(tmp)
+        index = np.array([sample_batched['id'], sample_batched['x'], sample_batched['y']])
+        
+        image_number = index[0]
+        
+        start_x = index[1] * PATCH_SIZE
+        end_x = start_x + PATCH_SIZE
+        
+        start_y = index[2] * PATCH_SIZE
+        end_y = start_y + PATCH_SIZE 
+        
+        tmp_img[image_number, start_x:end_x, start_y:end_y] = small_patch
+        
+        if index[1] == NUMBER_PATCHES_PER_IMAGE - 1 && index[2] == NUMBER_PATCHES_PER_IMAGE - 1:
+            save_image(tmp_img)
+            
+            tmp_img = np.zeros((IMAGE_SIZE, IMAGE_SIZE))
+        
+        
         
         if ind_batch % 10 == 0:
                 print(
@@ -64,8 +92,8 @@ def predict(model, dataloader):
                     )
                 )
         
-    masks = reconstruct_images(result, indices)
-    save_files(masks)
+#     masks = reconstruct_images(result, indices)
+#     save_files(masks)
         
 #     return result, indices, masks
         
