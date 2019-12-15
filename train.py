38ea49b99b6506f64ae3debaa872a02e82445bc6
tariq <t.kalim.95@gmail.com>
2019-12-15 10:53:49 +0100

    added checkpoints

--- train.py
+++ train.py
@@ -1,26 +1,60 @@
+import time
+from pathlib import Path
+
+import torch
 import torch.optim as optim
 
 LEARNING_RATE = 0.0001
+MODEL_NAME = "unet"
+CHECKPOINTS_DIR = "./checkpoints"
 
-def train(model, dataloader, epochs, criterion, model_weights=None, batch_size=1):
+
+def save_model(model, epoch, loss, save_dir):
+    model_name = MODEL_NAME
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    file_name = f"{timestr}_{model_name}_epoch_{epoch}_loss_{loss:03.3f}.pt"
+    Path(save_dir).mkdir(exist_ok=True)
+    file_path = Path(save_dir) / file_name
+    torch.save(model.state_dict(), str(file_path))
+
+
+def train(
+    model,
+    dataloader,
+    epochs,
+    criterion,
+    model_weights=None,
+    batch_size=1,
+    checkpoints_dir=CHECKPOINTS_DIR,
+):
+
+    cuda = torch.cuda.is_available()
+    if cuda:
+        model = model.to(device="cuda")
+        print("CUDA available")
+    else:
+        print("NO CUDA")
 
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-    
+
     for epoch in range(epochs):
         model.train()
         for ind_batch, sample_batched in enumerate(dataloader):
-            images = sample_batched['image']
-            groundtruths = sample_batched['groundtruth']
-            
+            images = sample_batched["image"]
+            groundtruths = sample_batched["groundtruth"]
+            if cuda:
+                images = images.to(device="cuda")
+                groundtruths = groundtruths.to(device="cuda")
+
             optimizer.zero_grad()
-       
+
             output = model(images)
-            
-            loss = criterion(output,groundtruths)
-        
+
+            loss = criterion(output, groundtruths)
+
             loss.require_grad = True
             loss.backward()
-            
+
             optimizer.step()
 
             if ind_batch % 10 == 0:
@@ -29,3 +63,8 @@ def train(model, dataloader, epochs, criterion, model_weights=None, batch_size=1
                         epoch, ind_batch, len(dataloader), loss
                     )
                 )
+        if epoch % 5 == 0:
+            save_model(
+                model=model, epoch=epoch, loss=loss.item(), save_dir=checkpoints_dir
+            )
+            print(f"model saved to {str(checkpoints_dir)}")
